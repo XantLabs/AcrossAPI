d065a739611c2006133fb36e5d40c633c0fe0522
Thomas Wang <dev@paced.me>
2017-03-19 03:44:25 +1100

    Another missing dip

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 
 from celery import Celery
 from celery.schedules import crontab
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
