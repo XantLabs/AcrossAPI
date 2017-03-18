52bebc948082b3af61f30d634d805bc5c227f451
Thomas Wang <dev@paced.me>
2017-03-19 04:27:24 +1100

    Added dict entry for views.

--- backend/__init__.py
+++ backend/__init__.py
@@ -144,6 +144,7 @@ def getTopN(n, userLat, userLon):
     # Generate distance for everything in the list. Make image list.
     for row in result:
         image = dict()
+        image['views'] = row.views
         image['distance'] = getDistance(float(userLat), float(userLon),
                                         float(row.lat),
                                         float(row.lon))
